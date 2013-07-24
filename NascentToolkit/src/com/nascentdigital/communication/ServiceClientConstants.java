a19be97cb6bd85a593cb177e2032d2fd710a29b5
ahinton-Nascent <ahinton@nascentdigital.com>
2013-07-24 11:36:06 -0400

    re-added constants

--- NascentToolkit/src/com/nascentdigital/communication/ServiceClientConstants.java
+++ NascentToolkit/src/com/nascentdigital/communication/ServiceClientConstants.java
@@ -8,5 +8,17 @@ public class ServiceClientConstants {
 	public static final int POOL_KEEP_ALIVE_SECONDS = 30;
 	public static final int DEFAULT_QUEUE_SIZE = 16;
 	public static final String UTF8_ENCODING = "UTF-8";
+	
+	public static final int SERVICE_RESPONSE_STATUS_CODE_ALERT_MESSAGE = -400;
+	public static final int SERVICE_RESPONSE_STATUS_CODE_CANCELLED = -300;
+	public static final int SERVICE_RESPONSE_STATUS_CODE_BAD_DATA = -200;
+	public static final int SERVICE_RESPONSE_STATUS_CODE_ARCHIVED = -100;
+	public static final int SERVICE_RESPONSE_STATUS_CODE_UNKNOWN = -1;
+	public static final int SSERVICE_RESPONSE_STATUS_CODE_NONE = 0;
+	public static final int SERVICE_RESPONSE_STATUS_CODE_SUCCESS = 200;
+	public static final int SSERVICE_RESPONSE_STATUS_CODE_BAD_PARAMETERS = 400;
+	public static final int SERVICE_RESPONSE_STATUS_CODE_UNAUTHORIZED = 401;
+	public static final int SERVICE_RESPONSE_STATUS_CODE_INTERNAL_SERVER_ERROR = 500;
+	public static final int SERVICE_RESPONSE_STATUS_CODE_SERVER_ERROR = 501;
 	// [endregion]
 }
